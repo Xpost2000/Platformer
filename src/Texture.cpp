693f805ffb4887ee12b4c91a97a664291d5d30b8
Xpost2000 <xpost2000cod@gmail.com>
2017-07-15 10:32:14 -0400

    Visitor Textures

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,5 +1,7 @@
 #include <GL/glew.h>
 #include "Texture.h"
+#include <iostream>
+#include "Bitmap.h"
 #include "IDevice.h"
 
 Texture::Texture(){
@@ -12,4 +14,44 @@ Texture::Texture(const std::shared_ptr<IDevice>& dev) : device(dev){
 
 Texture::~Texture(){
 	glDeleteTextures(1, &obj);
+}
+
+void Texture::bindTexture( TextureTarget t ){
+	try{
+	if(device == nullptr)
+		throw std::runtime_error("Device is nullptr");
+	device->bindTexture(t, *this);
+	}catch( const std::exception & e ){
+		std::cout << " Texture :: Exception : " << e.what() << std::endl;
+	}
+}
+
+void Texture::unbindTexture ( TextureTarget t ){
+	try{
+	if(device == nullptr)
+		throw std::runtime_error ("Device is nullptr");
+	device->unbindTexture(t);
+	}catch( const std::exception& e ){
+		std::cout << " Texture :: Exception : " << e.what() << std::endl;
+	}
+}
+
+void Texture::texImage2D(TextureTarget t, Bitmap& bm, GLint level, GLint border, GLenum type){
+	try{
+	if(device == nullptr)
+		throw std::runtime_error("Device is nullptr");
+	texImage2D(t, level, bm.get_format(), bm.get_width(), bm.get_height(), border, bm.get_format(), type, bm.get_data());
+	}catch(const std::exception& e ){
+		std::cout << " Texture :: Exception : " << e.what() << std::endl;
+	}
+}
+
+void Texture::texImage2D(TextureTarget t, GLint level, GLint inFmt, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid* data){
+	try{
+	if(device == nullptr)
+		throw std::runtime_error("Device is nullptr");
+	device->texImage2D(t, level, inFmt, width, height, border, format, type, data);
+	}catch (const std::exception& e){
+		std::cout << "Texture :: Exception : " << e.what() << std::endl;
+	}
 }
--- src/Texture.h
+++ src/Texture.h
@@ -1,6 +1,7 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 #include "IObjectHandle.h"
+#include "TextureParameter.h"
 #include "Comparable.h"
 #include "IDevice.h"
 #include <memory>
@@ -13,12 +14,18 @@ class Texture : public IObjectHandle, public Comparable<Texture> {
 	Texture(const Texture& other) = default;
 	~Texture();
 	const HandleType is_type() const{ return HandleType::HANDLE_TEXTURE; }
+	void bindTexture(TextureTarget);
+	void unbindTexture(TextureTarget);
+	// targ, bitmap, level, border, type, data
+	void texImage2D(TextureTarget, Bitmap&, GLint, GLint, GLenum);
+	void texImage2D(TextureTarget, GLint, GLint, GLsizei, GLsizei, GLint border, GLenum, GLenum, const GLvoid*);
+	void textureParameter(TextureTarget, TextureParameter, ParamValue);
 	
-		bool operator > ( Texture rhs ) { return true;}
-		bool operator < ( Texture rhs ) { return true;}
-		bool operator >= ( Texture rhs ) { return true;}
-		bool operator <= ( Texture rhs ) { return true; }
-		bool operator == ( Texture rhs ) { return (obj == rhs.obj);}
+	bool operator > ( Texture rhs ) { return true;}
+	bool operator < ( Texture rhs ) { return true;}
+	bool operator >= ( Texture rhs ) { return true;}
+	bool operator <= ( Texture rhs ) { return true; }
+	bool operator == ( Texture rhs ) { return (obj == rhs.obj);}
 	private:
 	std::shared_ptr<IDevice> device = nullptr;
 };
